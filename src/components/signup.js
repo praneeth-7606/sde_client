f4045460bc3d91145936227115a901c1b703625d
praneeth vedagiri <96403254+praneeth-7606@users.noreply.github.com>
2024-08-06 09:49:19 +0530

    Update signup.js

--- src/components/signup.js
+++ src/components/signup.js
@@ -30,7 +30,7 @@ function Signup() {
       e.preventDefault();
       try{
         const res=await axios.post(
-          `https://sde-server.onrender.com/api/route/auth/register`,{name,email,password,role,question});
+          `https://https://sde-server-1.onrender.com/api/route/auth/register`,{name,email,password,role,question});
         if(res && res.data.success){
           toast.success(res.data && res.data.message);
           await Promise.resolve();
@@ -47,7 +47,7 @@ function Signup() {
 
       }
     };
-    console.log("https://sde-server.onrender.com/api/route/auth/register")
+    console.log("https://https://sde-server-1.onrender.com/api/route/auth/register")
 
     
 
